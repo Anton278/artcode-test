d4d0024d67ed5789c51a690b538dc95f78ab65e8
anton278 <nakonechnyi.an@gmail.com>
2022-07-15 15:01:30 +0300

    refactored

--- src/asyncActions.ts
+++ src/asyncActions.ts
@@ -1,12 +1,14 @@
 import {
-    Service,
-    Brand,
-    Style,
-    getSelectedOptionsType,
+    getSelectedOptionsT,
     ParseLinkResponse,
+    Services,
+    getServicesT,
+    getBrandsT,
+    CustomResponse,
+    getStylesT,
 } from "./types";
 
-export const getSelectedOptions: getSelectedOptionsType = async (
+export const getSelectedOptions: getSelectedOptionsT = async (
     service,
     brand,
     style,
@@ -16,7 +18,7 @@ export const getSelectedOptions: getSelectedOptionsType = async (
 ): Promise<void> => {
     let url = `https://onboarding.art-code.team/api/test/v1/search/parse_link?service_slug=${service}&brand_slug=${brand}&style_slug=${style}`;
 
-    const response = await fetch(url);
+    const response: Response = await fetch(url);
     const result: ParseLinkResponse = await response.json();
 
     if (result.service) {
@@ -28,4 +30,41 @@ export const getSelectedOptions: getSelectedOptionsType = async (
     if (result.style) {
         setStyleValue(result.style.slug);
     }
+};
+
+export const getServices: getServicesT = async (
+    setServices,
+    setServiceValue
+): Promise<void> => {
+    const url = "https://onboarding.art-code.team/api/test/v1/search/terms";
+    const response: Response = await fetch(url);
+    const result: Services = await response.json();
+
+    setServices(result.data);
+    setServiceValue(result.data[0].slug);
+};
+
+export const getBrands: getBrandsT = async (
+    setBrands,
+    setBrandValue
+): Promise<void> => {
+    const url =
+        "https://onboarding.art-code.team/api/test/v1/search/brands_terms";
+    const response: Response = await fetch(url);
+    const result: CustomResponse = await response.json();
+
+    setBrands(result.data);
+    setBrandValue(result.data[0].slug);
+};
+
+export const getStyles: getStylesT = async (
+    setStyles,
+    setStyleValue
+): Promise<void> => {
+    const url = "https://onboarding.art-code.team/api/test/v1/search/styles";
+    const response: Response = await fetch(url);
+    const result: CustomResponse = await response.json();
+
+    setStyles(result.data);
+    setStyleValue(result.data[0].slug);
 };
