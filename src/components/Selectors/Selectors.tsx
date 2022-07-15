a63468825d9c3432412f5ed5b5f2e8ecc62c02f8
anton278 <nakonechnyi.an@gmail.com>
2022-07-15 15:07:24 +0300

    refactored

--- src/components/Selectors/Selectors.tsx
+++ src/components/Selectors/Selectors.tsx
@@ -1,65 +1,32 @@
 import "./Selectors.css";
+import { Item } from "../../types";
+import { useEffect, useState } from "react";
+import { useNavigate, useParams } from "react-router-dom";
 import {
-    Brand,
-    Service,
-    Style,
-    Services,
-    Brands,
-    Styles,
-    ParseLinkResponse,
-} from "../../types";
-import { useEffect, useRef, useState } from "react";
-import { useNavigate, useParams, useSearchParams } from "react-router-dom";
-import { getSelectedOptions } from "../../asyncActions";
+    getSelectedOptions,
+    getServices,
+    getBrands,
+    getStyles,
+} from "../../asyncActions";
 
 const Selectors: React.FC = () => {
-    const [services, setServices] = useState<Array<Service>>([]);
-    const [brands, setBrands] = useState<Array<Brand>>([]);
-    const [styles, setStyles] = useState<Array<Style>>([]);
+    const [services, setServices] = useState<Array<Item>>([]);
+    const [brands, setBrands] = useState<Array<Item>>([]);
+    const [styles, setStyles] = useState<Array<Item>>([]);
 
     const [serviceValue, setServiceValue] = useState("");
     const [brandValue, setBrandValue] = useState("");
     const [styleValue, setStyleValue] = useState("");
 
     const { service, brand, style } = useParams();
-    console.log(service, brand, style);
 
     const navigate = useNavigate();
 
-    const getServices = async (): Promise<void> => {
-        const url = "https://onboarding.art-code.team/api/test/v1/search/terms";
-        const response = await fetch(url);
-        const result: Services = await response.json();
-
-        setServices(result.data);
-        setServiceValue(result.data[0].slug);
-    };
-
-    const getBrands = async (): Promise<void> => {
-        const url =
-            "https://onboarding.art-code.team/api/test/v1/search/brands_terms";
-        const response = await fetch(url);
-        const result: Brands = await response.json();
-
-        setBrands(result.data);
-        setBrandValue(result.data[0].slug);
-    };
-
-    const getStyles = async (): Promise<void> => {
-        const url =
-            "https://onboarding.art-code.team/api/test/v1/search/styles";
-        const response = await fetch(url);
-        const result: Styles = await response.json();
-
-        setStyles(result.data);
-        setStyleValue(result.data[0].slug);
-    };
-
     useEffect(() => {
-        const fetchData = async () => {
-            await getServices();
-            await getBrands();
-            await getStyles();
+        const fetchData = async (): Promise<void> => {
+            await getServices(setServices, setServiceValue);
+            await getBrands(setBrands, setBrandValue);
+            await getStyles(setStyles, setStyleValue);
             await getSelectedOptions(
                 service,
                 brand,
@@ -82,7 +49,7 @@ const Selectors: React.FC = () => {
                         navigate(`/s-${e.target.value}/b-${brand}/st-${style}`);
                     }}
                 >
-                    {services.map((service: Service) => (
+                    {services.map((service: Item) => (
                         <option value={service.slug} key={service.id}>
                             {service.label}
                         </option>
@@ -99,7 +66,7 @@ const Selectors: React.FC = () => {
                         );
                     }}
                 >
-                    {brands.map((brand: Brand) => (
+                    {brands.map((brand: Item) => (
                         <option value={brand.slug} key={brand.id}>
                             {brand.label}
                         </option>
@@ -116,7 +83,7 @@ const Selectors: React.FC = () => {
                         );
                     }}
                 >
-                    {styles.map((style: Style) => (
+                    {styles.map((style: Item) => (
                         <option value={style.slug} key={style.id}>
                             {style.label}
                         </option>
